a3cf1eea5786392c7e14465d0770252a985688c3
megadewikp <megadewikp@gmail.com>
2020-03-28 01:34:52 +0700

    change continue shopping url

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -25,7 +25,7 @@ export default class Modal extends Component {
                                        <img src={img[0].image} className="img-fluid" alt="product"/>
                                        <h5>{title}</h5>
                                        <h5 className="text-muted">price : $ {price}</h5>
-                                       <Link to='/'>
+                                       <Link to='/products'>
                                            <ButtonContainer onClick={()=> closeModal()}>
                                                Continue Shopping
                                            </ButtonContainer>
